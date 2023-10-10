2f429f5ad7355d233ec51539b006349952ab4892
kilicheva <260495aa@gmail.com>
2023-10-10 11:05:58 +0300

    Add main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    # Домашняя страница
+    return render_template('замени_меня.html')
+
+
+@app.route('/contacts')
+def contacts():
+    # Контакты
+    adress = 'Мой адресс'
+    return render_template('замени_меня.html', adress=adress)
+
+
+@app.route('/buy')
+def buy():
+    # Оформление заказа
+    return render_template('замени_меня.html')
+
+
+@app.route('/specialoffer')
+def specialoffer():
+    # Cтраница с акцией
+    return render_template('замени_меня.html')
+
+
+if __name__ == "__main__":
+    app.run()
